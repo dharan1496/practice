146d2d0c87b028a7b39687b5e66a240b380f4ced
unknown <dharan1496@gmail.com>
2019-12-31 22:28:05 +0530

    first commit

---
+++ TestNG_Maven/src/test/java/testFile/google.java
@@ -0,0 +1,35 @@
+package testFile;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class google {
+	public WebDriver driver;
+	public static String url1 = null;
+//	@Test(threadPoolSize = 2, invocationCount = 2)
+//	public void main() throws Throwable {
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\chromedriver.exe");
+//		driver = new ChromeDriver();
+//		driver.get("http://www.google.com");
+//		System.out.println("first");
+//		Thread.sleep(2000);
+//		System.out.println(driver.getTitle() + " " + Thread.currentThread().getId());
+//		driver.quit();
+//	}
+
+	@Test
+	@Parameters({"url"})
+	public void main1(String url) throws Throwable {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\chromedriver.exe");
+		driver = new ChromeDriver();
+		url1 = url;
+		driver.get(url1);
+		System.out.println(driver.getTitle() + " " + Thread.currentThread().getId());
+		driver.quit();
+	}
+
+}
---
+++ TestNG_Maven/src/test/java/testFile/yahoo.java
@@ -0,0 +1,31 @@
+package testFile;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class yahoo {
+	public WebDriver driver;
+	@BeforeTest
+	public void setup() throws Throwable {
+	   
+	   
+	}
+
+	@Test
+	public void main() throws Throwable {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\chromedriver.exe");
+		driver = new ChromeDriver();
+	   driver.get("http://www.yahoo.com");
+	   System.out.println(driver.getTitle()+" "+Thread.currentThread().getId());
+	}
+
+	@AfterTest
+	public void teardown() throws Throwable {
+		driver.quit();
+	   
+	}
+
+}
